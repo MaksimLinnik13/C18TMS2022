4184218896be0cc52a47ba2f9d8fc74f8d48c003
maximlinnik <Underdog120339@gmail.com>
2022-10-30 21:19:44 +0100

    решение домашнего задания

---
+++ Lesson1/src/main/java/Task2.java
@@ -0,0 +1,14 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (String arg : args) {
+            int value = Integer.parseInt(arg);
+            if (value > 0) {
+                count = count + 1;
+            }
+        }
+        System.out.println("Количество положительных чисел " + count);
+        }
+}
+
+
