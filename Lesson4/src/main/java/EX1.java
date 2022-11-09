39a15da43e4e24e561338b1b37f76fddb3a58b48
maximlinnik <Underdog120339@gmail.com>
2022-11-09 04:01:23 +0100

    ДЗ Lesson 4 ( не все смог сделать )

---
+++ Lesson4/src/main/java/EX1.java
@@ -0,0 +1,9 @@
+public class EX1 {
+    //Задачи:
+//1) Используя тип данных char и операцию инкремента вывести на консоль все буквы английского алфавита
+    public static void main(String... args) {
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
